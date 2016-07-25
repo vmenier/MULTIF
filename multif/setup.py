87d145adee613285e47d158fd73e561581a4282d
Victorien Menier <menier@stanford.edu>
2016-07-25 11:29:06 -0700

    Added c99 compilation flag for meshutils

--- multif/setup.py
+++ multif/setup.py
@@ -1,5 +1,4 @@
 from distutils.core import setup, Extension
 
 setup(ext_modules=[Extension("_meshutils_module",
-      sources=["./meshutils/meshutils_py.c", "./meshutils/meshutils.c", "./meshutils/GMFio.c", "./meshutils/SU2io.c", "./meshutils/clean.c", "./meshutils/extraction.c", "./meshutils/libmesh6.c", "./meshutils/modules.c", "./meshutils/utils.c", "./meshutils/parser.c", "./meshutils/option.c", "./meshutils/mesh.c", "./meshutils/bspline3.c", "./meshutils/meshutils_py.i"])])
-
+      sources=["./meshutils/meshutils_py.c", "./meshutils/meshutils.c", "./meshutils/GMFio.c", "./meshutils/SU2io.c", "./meshutils/clean.c", "./meshutils/extraction.c", "./meshutils/libmesh6.c", "./meshutils/modules.c", "./meshutils/utils.c", "./meshutils/parser.c", "./meshutils/option.c", "./meshutils/mesh.c", "./meshutils/bspline3.c", "./meshutils/meshutils_py.i"],extra_compile_args=["-std=c99"])])
