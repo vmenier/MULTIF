6d28e55bb75647bf417f41d872e7e19ff7e4b3f2
Victorien Menier <menier@stanford.edu>
2017-07-31 16:21:32 -0700

    Fixed multif setup file (3)

--- multif/setup.py
+++ multif/setup.py
@@ -28,12 +28,13 @@ setup(ext_modules=[Extension("_meshutils_module",
  				 "./meshutils/meshutils_py.i", \
 			     "./meshutils/projection.c", \
  				 "./meshutils/GMSHio.c"],
-       extra_compile_args=["-std=c99","-Wno-unused-variable","-Wno-unused-result"])#,
+       extra_compile_args=["-std=c99","-Wno-unused-variable","-Wno-unused-result"]),
        Extension('_nozzle_module',
        sources = ['./meshutils/nozzle.cpp'],
 #       include_dirs=['/home/avery/Projects/Gmsh/gmsh-2.13.1-install/include/gmsh'],
 #       library_dirs=['/home/avery/Projects/Gmsh/gmsh-2.13.1-install/lib'],
-       libraries=['Gmsh'])
+       extra_compile_args=["-Wno-maybe-uninitialized","-std=c++11"],
+       libraries=['Gmsh'])])
 ])
 
 
