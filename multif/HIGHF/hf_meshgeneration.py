Fixed equivalent area computation

multif/HIGHF/hf_meshgeneration.py
@@ -4,7 +4,7 @@ import numpy as np
 from scipy.interpolate import splev, splrep
 from scipy import interpolate as itp
 import subprocess
-
+from multif import _meshutils_module
 
 def WriteGeo(FilNam, Ver, Spl, Lin, Loo, Phy, Siz, Bak, dim):
     
@@ -161,42 +161,7 @@ def WriteGeo(FilNam, Ver, Spl, Lin, Loo, Phy, Siz, Bak, dim):
 # params = a list with the following components:
 #          [z coord of cut at the throat, z coord of the flat exit, 0, 
 #           x-coordinate of inlet, x-coordinate of exit]
-def MF_GetRadius (x, fr1, fr2, fz, params):
-#<<<<<<< HEAD
-#    
-#    zcut0   = params[0]; # z crd of the cut at the throat
-#    zcut1   = params[1]; # z crd of the flat exit (bottom of the shovel)
-#    xthroat = params[3];
-#    xexit   = params[4];
-#    
-#    if ( isinstance(x, (list, tuple, np.ndarray)) ):
-#        rad = np.zeros(len(x));
-#        for i in range(0,len(x)):
-#            rad[i] = MF_GetRadius (x[i], fr1, fr2, fz, params);
-#        return rad;
-#    
-#    if ( x < -1e-12 ):
-#        return -1.0;
-#    
-#    r1 = fr1(x);
-#    r2 = fr2(x);
-#    
-#    
-#    z0  = fz(xthroat);
-#    r10 = fr1(xthroat);
-#    r20 = fr2(xthroat);
-#    # Theta is measured down from the vertical axis
-#    theta0 = math.acos((zcut0-z0)/r10);
-#    
-#    z1  = fz(xexit);
-#    r11 = fr1(xexit);
-#    r21 = fr2(xexit);
-#    # Theta is measured down from the vertical axis
-#    theta1 = math.acos((zcut1-z1)/r21);
-#    
-#    ftheta = itp.interp1d([xthroat,xexit],[theta0,theta1],kind='linear')
-#    theta = ftheta(x);
-#=======
+def MF_GetRadius_old (x, fr1, fr2, fz, params):
 	
 	zcut0   = params[0]; # z crd of the cut at the throat
 	zcut1   = params[1]; # z crd of the flat exit (bottom of the shovel)
@@ -227,13 +192,6 @@ def MF_GetRadius (x, fr1, fr2, fz, params):
 	ftheta = itp.interp1d([xthroat,xexit],[theta0,theta1],kind='linear')
 	theta = ftheta(x);
 
-
-#      print x, theta0*180/np.pi, theta1*180/np.pi, theta*180/np.pi
-#      
-#      if ( theta < np.pi/2 ):
-#              return -1.0;
-#      else :
-        
 	alp = (xexit-x)/(xexit-xthroat);
 	
 	area = 0.25*r1*r2*math.pi;
@@ -248,174 +206,281 @@ def MF_GetRadius (x, fr1, fr2, fz, params):
 	
 	return rad;
 	
-
+def MF_GetRadius (x, nozzle):
+    
+    if ( isinstance(x, (list, tuple, np.ndarray)) ):
+        rad = np.zeros(len(x));
+        for i in range(0,len(x)):
+        	rad[i] = MF_GetRadius (x[i],nozzle);
+        return rad;
+    
+    from .. import nozzle as noz
+    
+    geometry = noz.geometry;
+    
+    majoraxisTmp = geometry.Bspline(nozzle.wall.majoraxis.coefs);
+    minoraxisTmp = geometry.Bspline(nozzle.wall.minoraxis.coefs);
+    
+    fr1 = majoraxisTmp.radius
+    fr2 = minoraxisTmp.radius
+    
+    theta_in  = 1.572865;
+    theta_out = 1.855294;
+    
+    coefs_center = nozzle.wall.centerline.coefs;
+    
+    x_in   = coefs_center[0];
+    x_out  = coefs_center[len(coefs_center)/2-1];
+    
+    #--- Get x, r1, r2, zcenter
+    
+    r1 = fr1(x);
+    r2 = fr2(x);
+    
+    alp = (x-x_in)/(x_out-x_in);
+    
+    theta = alp*theta_out + (1.0-alp)*theta_in;
+    
+    #--- Compute area
+    
+    area_A = 0.5*theta*r1*r2; 
+    area_B = 0.5*(r1*r2*math.fabs(math.cos(theta)*math.sin(theta))); # area of triangle (orig,P(theta),orig-zcut)
+    area_C = 0.5*(math.pi-theta)*r1*r2 - area_B; # area below zcut
+        
+    area_tot = area_A+area_B + (1.0-alp) * area_C;
+    
+    #--- Verif area_C
+    x0 = r1*math.sin(theta);
+    y0 = r2*math.cos(theta);
+    #area_C_int = 0.5*r1*r2*((1.0-2*y0)*x0 - 0.5*math.sin(2*x0));
+    
+    #print "area_C %lf area_C_int %lf " % (area_C, area_C_int)
+    #sys.exit(1);
+    
+    area_tot = area_A+area_B + (1.0-alp) * area_C 
+    
+    rad = math.sqrt(2*area_tot/math.pi);
+    
+    return rad;    
 #
-#def MF_GetRadius (x, x_inp, fr1, fr2, fz, params):
+# 
+#def MF_DefineAxiSymCAD (FilNam, x_inp, fr1, fr2, fz, sizes, params):
 #    
-#    zcut0   = params[0]; # z crd of the cut at the throat
-#    zcut1   = params[1]; # z crd of the flat exit (bottom of the shovel)
-#    zcut2   = params[2]; # z coordinate of the top of the shovel
-#    xthroat = params[3];
-#    xexit   = params[4];
+#    NbrLnk = 400;
 #    
 #    
-#    if ( isinstance(x, (list, tuple, np.ndarray)) ):
-#        rad = np.zeros(len(x));
-#        for i in range(0,len(x)):
-#            rad[i] = MF_GetRadius (x[i], x_inp, fr1, fr2, fz, params);
-#        return rad;
+#    from .. import nozzle as noz
 #    
-#    if ( x < -1e-12 ):
-#        return -1.0;
-#        
-#    r1 = fr1(x);
-#    r2 = fr2(x);
+#    geometry = noz.geometry;
 #    
-#    if ( x < xthroat-1e-12 ):    
-#        area = r1*r2*math.pi;
-#        #return area;
-#        
-#    else:
-#                
-#        z0  = fz(xthroat);
-#        r10 = fr1(xthroat);
-#        r20 = fr2(xthroat);
-#        theta0 = math.acos((zcut0-z0)/r10);
-#        
-#        z1  = fz(xexit);
-#        r11 = fr1(xexit);
-#        r21 = fr2(xexit);
-#        theta1 = math.acos((zcut1-z1)/r21);
-#        
-#        ftheta = itp.interp1d([xthroat,xexit],[theta0,theta1],kind='linear')
-#        theta = ftheta(x);
-#        
-#        pis2 = 0.5*math.pi;
-#        
-#        if ( theta < pis2 ):
-#            return -1.0;
-#        else :
-#            
-#            alp = (xexit-x)/(xexit-xthroat);
-#            
-#            area = 0.25*r1*r2*math.pi;
-#            area = area + 0.5*r1*r2*(theta-pis2 - 0.5*math.sin(2*theta-math.pi));
-#            areab = 0.5*r1*r2*(math.pi - theta + 0.5*math.sin(2*theta-math.pi));
-#            
-#            area = 2*(area + alp*areab);
-#            
-#            #return area; 
-#            
-#    rad = math.sqrt(area/math.pi);
+#    majoraxisTmp = geometry.Bspline(nozzle.wall.majoraxis.coefs);
+#    minoraxisTmp = geometry.Bspline(nozzle.wall.minoraxis.coefs);
 #    
-#    return rad;
+#    fr1 = majoraxisTmp.radius
+#    fr2 = minoraxisTmp.radius
+#    
+#    coefs_center = nozzle.wall.centerline.coefs;
+#    
+#    x_in   = coefs_center[0];
+#    x_out  = coefs_center[len(coefs_center)/2-1];
+#    
+#    xmax    = params[5];
+#    ymax    = params[6];
+#    zmax    = params[7];
+#    
+#    hl0 = sizes[3];  # Edge size in the 'small' plume area
+#    hl1 = sizes[4];  # Edge size in the vicinity of the aircraft
+#    
+#    x = np.linspace(x_in, x_out, NbrLnk);
+#    #y = MF_GetRadius (x, x_inp, fr1, fr2, fz, params);
+#    y = MF_GetRadius (x,nozzle);
+#    
+#    
+#    Ver = [0];
+#    Spl = [0];
+#    Lin = [0];
+#    Loo = [0];
+#    Phy = [0];   # Physical groups
+#    Bak = [0];
+#    
+#    y0 = 0.8; 
+#    y2 = 0.8*y0;
+#    thickness = params[8];
+#    
+#    # Point sizes : 
+#    Siz = [sizes[0], sizes[1], sizes[2]] # inside nozzle, aircraft surf, farfield
+#    
+#    OutVid = np.zeros(100);
+#    OutLid = np.zeros(100);
+#    
+#    # --- Inner nozzle wall
+#    
+#    Spl.append([]);
+#    for i in range(0,NbrLnk):
+#        Ver.append([x[i], y[i], 0, 1]);
+#        Spl[-1].append(len(Ver)-1);
+#    
+#    OutLid[1] = len(Spl)-1;
+#    OutVid[8] = len(Ver)-1;
+#    OutVid[9] = 1;
+#    
+#    # --- Box
+#    
+#    Ver.append([0,0,0,1]);
+#    OutVid[1] = len(Ver)-1;
+#    
+#    Ver.append([xmax,0,0,3]);
+#    OutVid[2] = len(Ver)-1;
+#    
+#    Ver.append([xmax,zmax,0,3]);
+#    OutVid[3] = len(Ver)-1;
+#    
+#    Ver.append([0,zmax,0,3]);
+#    OutVid[4] = len(Ver)-1;
+#    
+#    Ver.append([0,y0,0,2]);
+#    OutVid[5] = len(Ver)-1;
+#    
+#    Ver.append([x_in,y2,0,2]);
+#    OutVid[6] = len(Ver)-1;
+#    
+#    Ver.append([x[-1],y[-1]+thickness,0,2]);
+#    OutVid[7] = len(Ver)-1;
+#    
+#    Ver.append([x[-1],0,0,1]);
+#    OutVid[10] = len(Ver)-1;
+#    
+#    # --- Add lines
+#    
+#    Spl.append([OutVid[8],OutVid[7]]);
+#    Spl.append([OutVid[7],OutVid[6],OutVid[5]]);
+#    Spl.append([OutVid[5],OutVid[4]]);
+#    Spl.append([OutVid[4],OutVid[3]]);
+#    Spl.append([OutVid[3],OutVid[2]]);
+#    Spl.append([OutVid[2],OutVid[10]]);
+#    Spl.append([OutVid[10],OutVid[1]]);
+#    Spl.append([OutVid[1],OutVid[9]]);
+#    
+#    # --- Add surface
+#    
+#    Loo.append([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+#    for i in range(1,7):
+#        Phy.append([i]); # physical lines
+#    Phy.append([7,8]);
+#    Phy.append([9]);
+#    
+#    # --- Define background size fields
+#    
+#    # size_in size_out xmax xmin ymax ymin zmax zmin
+#    hei = 0.45*(Ver[int(OutVid[5])][1]);
+#    Bak.append([hl1, 1000, 100, -100, Ver[int(OutVid[5])][1]+hei, -1, 1, -1]);
+#    
+#    # size_in size_out xmax xmin ymax ymin zmax zmin
+#    hei = 0.3*(Ver[int(OutVid[8])][1]);
+#    Bak.append([hl0, 1000, 100, Ver[int(OutVid[8])][0], Ver[int(OutVid[8])][1]+hei, -1, 1, -1]);
+#    
+#    # --- Write .geo file
+#    
+#    WriteGeo(FilNam, Ver, Spl, Lin, Loo, Phy, Siz, Bak, 2)
 #
-#
-#
- 
-def MF_DefineAxiSymCAD (FilNam, x_inp, fr1, fr2, fz, sizes, params):
+def HF_GenerateExitMesh(nozzle, xloc=-1):
+    
+    geonam = "nozzle_exit.geo";
+    HF_ExitPlaneCAD (geonam, nozzle, xloc);
+    
+    gmsh_executable = 'gmsh';
+    try :
+        cmd = [gmsh_executable, '-2', geonam, '-o', "nozzle_exit_hin.mesh"];
+        out = subprocess.check_output(cmd, stderr=subprocess.STDOUT, cwd=None)
+    except:
+        raise;
+    
+    
+def HF_ExitPlaneCAD (filename, nozzle, xloc):
+    
+    from .. import nozzle as noz
+    
+    geometry = noz.geometry;
+    
+    majoraxisTmp = geometry.Bspline(nozzle.wall.majoraxis.coefs);
+    minoraxisTmp = geometry.Bspline(nozzle.wall.minoraxis.coefs);
+    centerTmp    =  geometry.Bspline(nozzle.wall.centerline.coefs);    
+    
+    fr1 = majoraxisTmp.radius
+    fr2 = minoraxisTmp.radius
+    fz  = centerTmp.radius
+    
+    theta_in  = 1.572865;
+    theta_out = 1.855294;
+    
+    coefs_center = nozzle.wall.centerline.coefs;
+    
+    x_in   = coefs_center[0];
+    x_out  = coefs_center[len(coefs_center)/2-1];
+    
+    #--- Get x, r1, r2, zcenter
+    
+    if xloc > -0.1 :
+        x = xloc;
+    else :
+        h_in = 0.06;
+        x = x_out-h_in;
+    
+    r1 = fr1(x);
+    r2 = fr2(x);
+    zcen = fz(x);
+    
+    alp = (x-x_in)/(x_out-x_in);
     
     NbrLnk = 400;
     
-    xthroat = params[3];
-    xexit   = params[4];
-    xmax    = params[5];
-    ymax    = params[6];
-    zmax    = params[7];
+    #--- 
     
-    hl0 = sizes[3];  # Edge size in the 'small' plume area
-    hl1 = sizes[4];  # Edge size in the vicinity of the aircraft
-    
-    x = np.linspace(0, xexit, NbrLnk);
-    y = MF_GetRadius (x, x_inp, fr1, fr2, fz, params);
+    theta = alp*theta_out + (1.0-alp)*theta_in;
     
     Ver = [0];
     Spl = [0];
     Lin = [0];
     Loo = [0];
-    Phy = [0];   # Physical groups
+    Phy = [0];
     Bak = [0];
+    dim = 3;
     
-    y0 = 0.8; 
-    y2 = 0.8*y0;
-    thickness = params[8];
+    Siz = [0.007, 0.008];
     
-    # Point sizes : 
-    Siz = [sizes[0], sizes[1], sizes[2]] # inside nozzle, aircraft surf, farfield
-    
-    OutVid = np.zeros(100);
-    OutLid = np.zeros(100);
-    
-    # --- Inner nozzle wall
-    
-    Spl.append([]);
-    for i in range(0,NbrLnk):
-        Ver.append([x[i], y[i], 0, 1]);
+    # Upper part
+    ttab = np.linspace(0.0,theta, NbrLnk);
+    Spl.append([])
+    for i in range(len(ttab)):
+        Ver.append([x,r1*math.sin(ttab[i]), zcen+r2*math.cos(ttab[i]),1]);
         Spl[-1].append(len(Ver)-1);
     
-    OutLid[1] = len(Spl)-1;
-    OutVid[8] = len(Ver)-1;
-    OutVid[9] = 1;
+    zcut = r2*math.cos(theta);
     
-    # --- Box
+    # Lower part
+    ttab = np.linspace(theta, math.pi, NbrLnk);
+    Spl.append([])
+    Spl[-1].append(len(Ver)-1);
     
-    Ver.append([0,0,0,1]);
-    OutVid[1] = len(Ver)-1;
+    for i in range(1,len(ttab)):        
+        z = zcen+alp*zcut+(1.0-alp)*(r2*math.cos(ttab[i]));
+        Ver.append([x,r1*math.sin(ttab[i]),z,1]);
+        Spl[-1].append(len(Ver)-1);
+        
+    # Line
+    Spl.append([]);
+    Spl[-1] = [len(Ver)-1,1];
     
-    Ver.append([xmax,0,0,3]);
-    OutVid[2] = len(Ver)-1;
+    # Loop
+    Loo.append([]);
+    Loo[-1] = [1,2,3];
     
-    Ver.append([xmax,zmax,0,3]);
-    OutVid[3] = len(Ver)-1;
+    # Physical
+    Phy.append([1]);
+        
+    WriteGeo(filename, Ver, Spl, Lin, Loo, Phy, Siz, Bak, 3) ;
     
-    Ver.append([0,zmax,0,3]);
-    OutVid[4] = len(Ver)-1;
     
-    Ver.append([0,y0,0,2]);
-    OutVid[5] = len(Ver)-1;
-    
-    Ver.append([xthroat,y2,0,2]);
-    OutVid[6] = len(Ver)-1;
-    
-    Ver.append([x[-1],y[-1]+thickness,0,2]);
-    OutVid[7] = len(Ver)-1;
-    
-    Ver.append([x[-1],0,0,1]);
-    OutVid[10] = len(Ver)-1;
-    
-    # --- Add lines
-    
-    Spl.append([OutVid[8],OutVid[7]]);
-    Spl.append([OutVid[7],OutVid[6],OutVid[5]]);
-    Spl.append([OutVid[5],OutVid[4]]);
-    Spl.append([OutVid[4],OutVid[3]]);
-    Spl.append([OutVid[3],OutVid[2]]);
-    Spl.append([OutVid[2],OutVid[10]]);
-    Spl.append([OutVid[10],OutVid[1]]);
-    Spl.append([OutVid[1],OutVid[9]]);
-    
-    # --- Add surface
-    
-    Loo.append([1, 2, 3, 4, 5, 6, 7, 8, 9]);
-    for i in range(1,7):
-        Phy.append([i]); # physical lines
-    Phy.append([7,8]);
-    Phy.append([9]);
-    
-    # --- Define background size fields
-    
-    # size_in size_out xmax xmin ymax ymin zmax zmin
-    hei = 0.45*(Ver[int(OutVid[5])][1]);
-    Bak.append([hl1, 1000, 100, -100, Ver[int(OutVid[5])][1]+hei, -1, 1, -1]);
-    
-    # size_in size_out xmax xmin ymax ymin zmax zmin
-    hei = 0.3*(Ver[int(OutVid[8])][1]);
-    Bak.append([hl0, 1000, 100, Ver[int(OutVid[8])][0], Ver[int(OutVid[8])][1]+hei, -1, 1, -1]);
-    
-    # --- Write .geo file
-    
-    WriteGeo(FilNam, Ver, Spl, Lin, Loo, Phy, Siz, Bak, 2)
-
 
 def HF_DefineCAD (FilNam, x_inp, fr1, fr2, fz, sizes, params):
     
@@ -1514,8 +1579,138 @@ def LoadBSplineParameters(FilNam):
         
         
     return Knots_center, Coefs_center, Knots_r1, Coefs_r1, Knots_r2, Coefs_r2;
+
+
+def SwitchTriangleIdx(tri):
     
+    vid = tri;
     
+    if vid[1] < vid[0] :
+        if vid[2] < vid[1] :
+            tri[0] = vid[2];
+            tri[1] = vid[0];
+            tri[2] = vid[1];
+        else:
+            tri[0] = vid[1];
+            tri[1] = vid[2];
+            tri[2] = vid[0];
+            
+    elif vid[2] < vid[0] :
+        tri[0] = vid[2];
+        tri[1] = vid[0];
+        tri[2] = vid[1];
+        
+    return tri;
+
+
+
+def GetEdgeInwardNormal(crd0, crd1):
+    
+    vno = [0.0,0.0]
+    
+    vno[0] = crd0[1] - crd1[1];
+    vno[1] = crd1[0] - crd0[0];
+    
+    return vno;
+
+def GetTriangleArea3D(Coord):
+    
+    crd0 = Coord[0];
+    crd1 = Coord[1];
+    crd2 = Coord[2];
+    
+    x = [0.0,0.0,0.0];
+    y = [0.0,0.0,0.0];
+    z = [0.0,0.0,0.0];
+    
+    x[0] = 0.;
+    y[0] = 0.;
+    z[0] = 0.;
+    
+    x[1] = crd1[0] - crd0[0];
+    y[1] = crd1[1] - crd0[1];
+    z[1] = crd1[2] - crd0[2];
+    
+    x[2] = crd2[0] - crd0[0];
+    y[2] = crd2[1] - crd0[1];
+    z[2] = crd2[2] - crd0[2];
+    
+    #--- P0P1/\P0P2
+    vnx =   y[1]*z[2] - y[2]*z[1];
+    vny = - x[1]*z[2] + x[2]*z[1];
+    vnz =   x[1]*y[2] - x[2]*y[1];
+    
+    #--- Area of the face : 2*Air = ||u1 vect u2||
+    Area = 0.5*math.sqrt(math.fabs(vnx*vnx + vny*vny + vnz*vnz));
+    
+    return Area;
+
+def GetTriangleArea2D(Coord):
+    
+    nDim = 3;
+    
+    a = [0.0,0.0,0.0];
+    b = [0.0,0.0,0.0];
+    
+    vno = [[0.0,0.0,0.0],[0.0,0.0,0.0]]
+    
+    Coord_0 = Coord[0];
+    Coord_1 = Coord[1];
+    Coord_2 = Coord[2];
+    
+    vno[0] = GetEdgeInwardNormal(Coord_1, Coord_2);
+    vno[1] = GetEdgeInwardNormal(Coord_2, Coord_0);
+    
+    print vno
+    
+    Area = math.fabs(vno[0][0]*vno[1][1]-vno[0][1]*vno[1][0]);
+    
+    return Area;
+  
+
+
+def Get2DMeshArea(MshNam):
+    
+    #--- Load mesh
+    
+    SolNam = ""
+    
+    Ver = [];
+    Tri = [];
+    Tet = [];
+    Edg = [];
+    Sol = [];
+    
+    _meshutils_module.py_ReadMesh(MshNam, SolNam, Ver, Tri, Tet, Edg, Sol);
+    
+    NbrTri = len(Tri)/4;
+    Tri = np.reshape(Tri,(NbrTri, 4)).astype(int);
+    
+    NbrVer = len(Ver)/3;
+    Ver = np.reshape(Ver,(NbrVer, 3));
+    
+    #--- Compute area
+    
+    NbrTri = len(Tri);
+    
+    Coord = [[0.0,0.0,0.0],[0.0,0.0,0.0],[0.0,0.0,0.0]]
+    
+    AreaTot = 0.0;
+    
+    for iTri in range(0,NbrTri):
+        
+        for j in range(3):
+            iVer = Tri[iTri][j]-1;
+            Coord[j]  = Ver[iVer]; 
+
+        area = GetTriangleArea3D(Coord);
+        iVer = Tri[iTri][0]-1;
+        
+        AreaTot += area;
+        
+    return AreaTot;
+ 
+  
 def HF_GenerateMesh_Deform(nozzle):
     
     from .. import _meshutils_module
@@ -1562,55 +1757,7 @@ def HF_GenerateMesh_Deform(nozzle):
      
     _meshutils_module.py_ConvertGMFToSU2(basNamGMF,"",basNamSU2);
     
-    #centerline_spl.dat
-    #r1_spl.dat
-    #r2_spl.dat
     
-    ####print "WRITE"
-    ####
-    ####print Coefs_center
-    ####
-    ####
-    ####hdl  = open("centerline_knots.dat", 'w');
-    ####hdl.write("%d\n"%len(Knots_center));
-    ####for j in range(len(Knots_center)):
-    ####    hdl.write("%lf \n" % Knots_center[j]);
-    ####hdl.close();
-    ####
-    ####hdl  = open("centerline_coefs.dat", 'w');
-    ####hdl.write("%d\n"%len(Coefs_center));
-    ####for j in range(len(Coefs_center)):
-    ####    hdl.write("%lf \n" % Coefs_center[j]);
-    ####hdl.close();
-    ####
-    ####
-    ####hdl  = open("r1_knots.dat", 'w');
-    ####hdl.write("%d\n"%len(Knots_r1));
-    ####for j in range(len(Knots_r1)):
-    ####    hdl.write("%lf \n" % Knots_r1[j]);
-    ####hdl.close();
-    ####
-    ####hdl  = open("r1_coefs.dat", 'w');
-    ####hdl.write("%d\n"%len(Coefs_r1));
-    ####for j in range(len(Coefs_r1)):
-    ####    hdl.write("%lf \n" % Coefs_r1[j]);
-    ####hdl.close();
-    ####
-    ####hdl  = open("r2_knots.dat", 'w');
-    ####hdl.write("%d\n"%len(Knots_r2));
-    ####for j in range(len(Knots_r2)):
-    ####    hdl.write("%lf \n" % Knots_r2[j]);
-    ####hdl.close();
-    ####
-    ####hdl  = open("r2_coefs.dat", 'w');
-    ####hdl.write("%d\n"%len(Coefs_r2));
-    ####for j in range(len(Coefs_r2)):
-    ####    hdl.write("%lf \n" % Coefs_r2[j]);
-    ####hdl.close();
-
-    #from shutil import copyfile
-    #copyfile("../nozzle.su2", "./nozzle.su2")
-    #return;
     
     # --- Call SU2_DEF to deform baseline mesh
     # --- Setup config file

multif/HIGHF/hf_run.py
@@ -23,225 +23,265 @@ def CheckOptions (nozzle):
         sys.exit(0);
     
 def Run( nozzle, output = 'verbose', writeToFile=1 ):
-	
-	
-	
-	#HF_GenerateMesh_Deform(nozzle);
-	#
-	#return;
-	
-	# Introduction to this function and available data
-	print
-	print '*****'
-	print
-	print 'MISSION:'
-	print nozzle.mission.__dict__
-	print
-	
-	print 'CFD-related information:'
-	print nozzle.cfd.__dict__
-	print
-	
-	print 'Fluid-related information:'
-	print nozzle.fluid.__dict__
-	print
-	
-	print 'Exterior environment information:'
-	print nozzle.environment.__dict__
-	print
-	
-	print 'INLET information:'
-	print nozzle.inlet.__dict__
-	print
-	
-	print 'NOZZLE INTERIOR WALL:'
-	print 'Nozzle wall centerline geometry:'
-	print nozzle.wall.centerline.geometry.__dict__
-	print 'Nozzle wall major axis geometry:'
-	print nozzle.wall.majoraxis.geometry.__dict__
-	print 'Nozzle wall minor axis geometry:'
-	print nozzle.wall.minoraxis.geometry.__dict__
-	print 'Nozzle length: %f' % nozzle.length
-	print 'Nozzle wall struct:'
-	print nozzle.wall.__dict__
-	print 'Nozzle wall shovel exit height: %f' % nozzle.wall.shovel_height
-	print 'Nozzle wall shovel inlet start angle: %f' % nozzle.wall.shovel_start_angle
-	print
-	
-	print 'WALL LAYERS:'
-	for i in range(len(nozzle.wall.layer)):
-	    print 'Nozzle wall layer: %s' % nozzle.wall.layer[i].name
-	    print nozzle.wall.layer[i].__dict__
-	print
-	
-	print 'STRINGERS:'
-	print nozzle.stringers.__dict__
-	print 'Example access of stringers height definition for 2nd stringer:'
-	print nozzle.stringers.height[1].__dict__
-	print 'Example access of stringers thickness definition for 2nd stringer:'
-	print nozzle.stringers.thickness[1].__dict__
-	print
-	
-	print 'BAFFLES:'
-	print nozzle.baffles.__dict__
-	print
-	
-	print 'EXTERIOR:'
-	print nozzle.exterior.__dict__
-	print
-	
-	print 'Entire NOZZLE struct:'
-	for k in nozzle.__dict__:
-	    print k
-	    print nozzle.__dict__[k]
-	    print
-	    
-	# Obtain mass and volume
-	if 'MASS' in nozzle.responses or 'VOLUME' in nozzle.responses:
-	    # XXX Calculate mass and volume here from CAD geometry & material prop.
-	    # Perhaps modify existing function:
-	    # volume, mass = nozzlemod.geometry.calcVolumeAndMass(nozzle)
-	    volume = [0,0];
-	    mass = [0,0];
-	    if 'MASS' in nozzle.responses:
-	        nozzle.responses['MASS'] = np.sum(mass)
-	    if 'VOLUME' in nozzle.responses:
-	        nozzle.responses['VOLUME'] = np.sum(volume)
-	
-	# Calculate mass gradients if necessary
-	if 'MASS' in nozzle.gradients and nozzle.gradients['MASS'] is not None:
-	    if ( nozzle.gradientsMethod == 'ADJOINT' ):
-	        # XXX Calculate derivative of mass here, using FD when adjoint is 
-	        # used for fluid. Perhaps modify existing function:
-	        # nozzle.gradients['MASS'] = nozzlemod.geometry.calcMassGradientsFD(nozzle,1e-8)
-	        nozzle.gradients['MASS'] = [0.]*len(nozzle.derivativesDV);
-	    elif ( nozzle.gradientsMethod == 'FINITE_DIFF' ):
-	        # XXX Calculate derivative of mass here using FD.
-	        # Perhaps modify existing function:
-	        # nozzle.gradients['MASS'] = \
-	        #   nozzlemod.geometry.calcMassGradientsFD(nozzle,nozzle.fd_step_size)  
-	        nozzle.gradients['MASS'] = [0.]*len(nozzle.derivativesDV);          
-	    else:
-		    sys.stderr.write('  ## ERROR : Unknown gradients computation '
-		      'method.\n');
-		    sys.exit(1);
-	
-	# Calculate volume gradients if necessary
-	if 'VOLUME' in nozzle.gradients and nozzle.gradients['VOLUME'] is not None:
-	    sys.stderr.write('\n ## ERROR : gradients for VOLUME are not supported\n\n');
-	    sys.exit(1);
-	    
-	# Obtain mass of wall and gradients only if requested
-	if 'MASS_WALL_ONLY' in nozzle.responses:
-	    # XXX Calculate mass of nozzle wall only (no stringers, no baffles)
-	    # from CAD geometry & material properties
-	    # This QoI is not very important, so feel free to skip this.
-	    nozzle.responses['MASS_WALL_ONLY'] = 0.
-	    
-	if 'MASS_WALL_ONLY' in nozzle.gradients and nozzle.gradients['MASS_WALL_ONLY'] is not None:
-	    sys.stderr.write('\n ## ERROR : gradients for MASS_WALL_ONLY are not supported\n\n');
-	    sys.exit(1);
-	
-	# Run aero-thermal-structural analysis if necessary
-	runAeroThermalStructuralProblem = 0;
-	for k in nozzle.responses:
-	    if k not in ['MASS','VOLUME','MASS_WALL_ONLY']:
-	        runAeroThermalStructuralProblem = 1;    
-	
-	# Run aero-thermal-structural gradient analysis if necessary
-	if nozzle.gradientsFlag == 1:
-	    runAeroThermalStructuralGradients = 0;        
-	    for k in nozzle.gradients:
-	        if k not in ['MASS','VOLUME','MASS_WALL_ONLY']:
-	            if nozzle.gradients[k] is not None:
-	                runAeroThermalStructuralGradients = 1;
-	
-	if runAeroThermalStructuralProblem:
-	    
-	    # Run aero analysis (and thrust adjoint if necessary)  
-	    CheckSU2Version(nozzle);	
-	    CheckOptions(nozzle);
-	    
-	    curDir = os.path.dirname(os.path.realpath(__file__));	
-	    if nozzle.runDir != '':
-	        os.chdir(nozzle.runDir);
-	    # XXX Ensure high-fidelity SU2 analysis runs correctly
-	    gradCalc = HF_runSU2(nozzle);
-		
-	   # Run thermal/structural analyses
-	    if nozzle.thermalFlag == 1 or nozzle.structuralFlag == 1:
-	        # XXX Ensure high-fidelity AEROS analysis runs correctly
-	        try : 
-	            runAEROS(nozzle, output);
-	        except:
-	           sys.stdout.write("  ## WARNING: CALL TO AERO-S IGNORED.\n");
-	           
-		
-	    # Assign aero QoI if required
-	    # XXX Fill in this PostProcess function to return correct outputs
-	    hf_postprocessing.PostProcess(nozzle, output);
-	    
-	    # Assign thermal/structural QoI if required
-	    if nozzle.thermalFlag == 1 or nozzle.structuralFlag == 1:
-	        # XXX Ensure AEROS post-processing returns correct outputs
-	        #AEROSpostprocessing.PostProcess(nozzle, output);      
-	        try : 
-	           AEROSpostprocessing.PostProcess(nozzle, output);    
-	        except:
-	           sys.stdout.write("  ## WARNING: CALL TO AERO-S IGNORED.\n");       
-	    
-	    # Calculate gradients if necessary
-	    if nozzle.gradientsFlag == 1 and runAeroThermalStructuralGradients:
-	
-	        if ( nozzle.gradientsMethod == 'ADJOINT' ):
-				
-	            if gradCalc == 0: # i.e. failed adjoint calculation, use finite differences
-	                # XXX Ensure FD gradients work for 3-D nozzle. If this
-	                # function needs to be changed, ensure it is also changed
-	                # in the other 2 spots below too.
-	                multif.gradients.calcGradientsFD(nozzle,nozzle.fd_step_size,output);
-	            else:
-	                # Check for other required gradients
-	                otherRequiredGradients = 0;
-	                for k in nozzle.gradients:
-	                    if k not in ['MASS','VOLUME','MASS_WALL_ONLY','THRUST']:
-	                        otherRequiredGradients = 1;
-	                        sys.stderr.write(' ## WARNING: QoI gradients desired using ADJOINT '
-	                          'method which do not have an associated adjoint calculation.\n'
-	                          ' Namely: %s. The current implementation requires finite '
-	                          'differencing across the aero analysis, so this method is '
-	                          'equivalent in computational cost to choosing the FINITE_DIFF'
-	                          ' method\n' % k);
-	                # Do finite difference for other QoI if there are any, but use
-	                # adjoint gradients for thrust
-	                if otherRequiredGradients:
-	                    saveThrustGradients = nozzle.gradients['THRUST'];
-	                    nozzle.gradients['THRUST'] = None;
-	                    multif.gradients.calcGradientsFD(nozzle,nozzle.fd_step_size,output);
-	                    nozzle.gradients['THRUST'] = saveThrustGradients;
-	                    
-	        elif ( nozzle.gradientsMethod == 'FINITE_DIFF' ):
-	        
-	            multif.gradients.calcGradientsFD(nozzle,nozzle.fd_step_size,output);  
-	                     
-	        else:
-			    sys.stderr.write('  ## ERROR : Unknown gradients computation '
-			      'method.\n');
-			    sys.exit(1);
-			    
-		 # Write separate gradients file
-	        gradFile = open(nozzle.gradientsFile,'w');
-	        for k in nozzle.outputTags:
-			    np.savetxt(gradFile,nozzle.gradients[k]);
-	        gradFile.close();   			
-	
-	# Write data
-	if writeToFile:
-	    if nozzle.outputFormat == 'PLAIN':
-	        nozzle.WriteOutputFunctions_Plain();
-	    else:
-	        nozzle.WriteOutputFunctions_Dakota();
-	
-	return 0;
+    
+    
+    
+    #HF_GenerateMesh_Deform(nozzle);
+    #
+    #return;
+    
+    # Introduction to this function and available data
+    print
+    print '*****'
+    print
+    print 'MISSION:'
+    print nozzle.mission.__dict__
+    print
+    
+    print 'CFD-related information:'
+    print nozzle.cfd.__dict__
+    print
+    
+    print 'Fluid-related information:'
+    print nozzle.fluid.__dict__
+    print
+    
+    print 'Exterior environment information:'
+    print nozzle.environment.__dict__
+    print
+    
+    print 'INLET information:'
+    print nozzle.inlet.__dict__
+    print
+    
+    print 'NOZZLE INTERIOR WALL:'
+    print 'Nozzle wall centerline geometry:'
+    print nozzle.wall.centerline.geometry.__dict__
+    print 'Nozzle wall major axis geometry:'
+    print nozzle.wall.majoraxis.geometry.__dict__
+    print 'Nozzle wall minor axis geometry:'
+    print nozzle.wall.minoraxis.geometry.__dict__
+    print 'Nozzle length: %f' % nozzle.length
+    print 'Nozzle wall struct:'
+    print nozzle.wall.__dict__
+    print 'Nozzle wall shovel exit height: %f' % nozzle.wall.shovel_height
+    print 'Nozzle wall shovel inlet start angle: %f' % nozzle.wall.shovel_start_angle
+    print
+    
+    print 'WALL LAYERS:'
+    for i in range(len(nozzle.wall.layer)):
+        print 'Nozzle wall layer: %s' % nozzle.wall.layer[i].name
+        print nozzle.wall.layer[i].__dict__
+    print
+    
+    print 'STRINGERS:'
+    print nozzle.stringers.__dict__
+    print 'Example access of stringers height definition for 2nd stringer:'
+    print nozzle.stringers.height[1].__dict__
+    print 'Example access of stringers thickness definition for 2nd stringer:'
+    print nozzle.stringers.thickness[1].__dict__
+    print
+    
+    print 'BAFFLES:'
+    print nozzle.baffles.__dict__
+    print
+    
+    print 'EXTERIOR:'
+    print nozzle.exterior.__dict__
+    print
+    
+    print 'Entire NOZZLE struct:'
+    for k in nozzle.__dict__:
+        print k
+        print nozzle.__dict__[k]
+        print
+        
+    # Obtain mass and volume
+    if 'MASS' in nozzle.responses or 'VOLUME' in nozzle.responses:
+        # XXX Calculate mass and volume here from CAD geometry & material prop.
+        # Perhaps modify existing function:
+        # volume, mass = nozzlemod.geometry.calcVolumeAndMass(nozzle)
+        volume = [0,0];
+        mass = [0,0];
+        if 'MASS' in nozzle.responses:
+            nozzle.responses['MASS'] = np.sum(mass)
+        if 'VOLUME' in nozzle.responses:
+            nozzle.responses['VOLUME'] = np.sum(volume)
+    
+    # Calculate mass gradients if necessary
+    if 'MASS' in nozzle.gradients and nozzle.gradients['MASS'] is not None:
+        if ( nozzle.gradientsMethod == 'ADJOINT' ):
+            # XXX Calculate derivative of mass here, using FD when adjoint is 
+            # used for fluid. Perhaps modify existing function:
+            # nozzle.gradients['MASS'] = nozzlemod.geometry.calcMassGradientsFD(nozzle,1e-8)
+            nozzle.gradients['MASS'] = [0.]*len(nozzle.derivativesDV);
+        elif ( nozzle.gradientsMethod == 'FINITE_DIFF' ):
+            # XXX Calculate derivative of mass here using FD.
+            # Perhaps modify existing function:
+            # nozzle.gradients['MASS'] = \
+            #   nozzlemod.geometry.calcMassGradientsFD(nozzle,nozzle.fd_step_size)  
+            nozzle.gradients['MASS'] = [0.]*len(nozzle.derivativesDV);          
+        else:
+            sys.stderr.write('  ## ERROR : Unknown gradients computation '
+              'method.\n');
+            sys.exit(1);
+    
+    # Calculate volume gradients if necessary
+    if 'VOLUME' in nozzle.gradients and nozzle.gradients['VOLUME'] is not None:
+        sys.stderr.write('\n ## ERROR : gradients for VOLUME are not supported\n\n');
+        sys.exit(1);
+        
+    # Obtain mass of wall and gradients only if requested
+    if 'MASS_WALL_ONLY' in nozzle.responses:
+        # XXX Calculate mass of nozzle wall only (no stringers, no baffles)
+        # from CAD geometry & material properties
+        # This QoI is not very important, so feel free to skip this.
+        nozzle.responses['MASS_WALL_ONLY'] = 0.
+        
+    if 'MASS_WALL_ONLY' in nozzle.gradients and nozzle.gradients['MASS_WALL_ONLY'] is not None:
+        sys.stderr.write('\n ## ERROR : gradients for MASS_WALL_ONLY are not supported\n\n');
+        sys.exit(1);
+    
+    # Run aero-thermal-structural analysis if necessary
+    runAeroThermalStructuralProblem = 0;
+    for k in nozzle.responses:
+        if k not in ['MASS','VOLUME','MASS_WALL_ONLY']:
+            runAeroThermalStructuralProblem = 1;    
+    
+    # Run aero-thermal-structural gradient analysis if necessary
+    if nozzle.gradientsFlag == 1:
+        runAeroThermalStructuralGradients = 0;        
+        for k in nozzle.gradients:
+            if k not in ['MASS','VOLUME','MASS_WALL_ONLY']:
+                if nozzle.gradients[k] is not None:
+                    runAeroThermalStructuralGradients = 1;
+    
+    if runAeroThermalStructuralProblem:
+        
+        # Run aero analysis (and thrust adjoint if necessary)  
+        CheckSU2Version(nozzle);    
+        CheckOptions(nozzle);
+        
+        curDir = os.path.dirname(os.path.realpath(__file__));    
+        if nozzle.runDir != '':
+            os.chdir(nozzle.runDir);
+        # XXX Ensure high-fidelity SU2 analysis runs correctly
+        gradCalc = HF_runSU2(nozzle);
+        
+        # Run thermal/structural analyses
+        if nozzle.thermalFlag == 1 or nozzle.structuralFlag == 1:
+            # XXX Ensure high-fidelity AEROS analysis runs correctly
+            try : 
+                runAEROS(nozzle, output);
+            except:
+               sys.stdout.write("  ## WARNING: CALL TO AERO-S IGNORED.\n");
+               
+        
+        # Assign aero QoI if required
+        # XXX Fill in this PostProcess function to return correct outputs
+
+        # ------- BEGIN HACK VERIF EQUIV AREA
+        # -> Creates N meshes of nozzle cross sections and compares areas with the analytic ones
+        
+        #import multif
+        #
+        #coefs_center = nozzle.wall.centerline.coefs;
+        #
+        #x_in   = coefs_center[0];
+        #x_out  = coefs_center[len(coefs_center)/2-1];
+        #
+        #xtab = np.linspace(x_in,x_out,15);
+        #
+        #area_tab = [];
+        #
+        #for i in range(len(xtab)):
+        #    
+        #    multif.HIGHF.HF_GenerateExitMesh(nozzle, xtab[i]);
+        #    area_msh = multif.HIGHF.Get2DMeshArea("nozzle_exit_hin.mesh");
+        #    
+        #    rad = MF_GetRadius (xtab[i],nozzle);
+        #    area_rad = 0.5*rad*rad*math.pi;
+        #    
+        #    print "x %lf area_msh %le (rad %lf) area_rad %le (rad %lf)" % (xtab[i],area_msh,math.sqrt(2*area_msh/math.pi),area_rad, rad)
+        #    
+        #    area_tab.append([xtab[i],area_msh ,math.sqrt(2*area_msh/math.pi),area_rad, rad]);
+        #    
+        #
+        #for i in range(len(area_tab)):
+        #    print "x=%lf area_msh %le area_rad %le: " % (area_tab[i][0],area_tab[i][1],area_tab[i][2])
+        #
+        #hdl=open("equivarea.dat", "w");
+        #for i in range(len(area_tab)):
+        #    hdl.write("%le %le %le %le %le\n" % (area_tab[i][0],area_tab[i][1],area_tab[i][2],area_tab[i][3],area_tab[i][4]))
+        #hdl.close();
+        #sys.exit(1);
+        
+        # ------- END HACK VERIF EQUIV AREA
+        
+        
+        hf_postprocessing.PostProcess(nozzle, output);
+        
+        # Assign thermal/structural QoI if required
+        if nozzle.thermalFlag == 1 or nozzle.structuralFlag == 1:
+            # XXX Ensure AEROS post-processing returns correct outputs
+            #AEROSpostprocessing.PostProcess(nozzle, output);      
+            try : 
+               AEROSpostprocessing.PostProcess(nozzle, output);    
+            except:
+               sys.stdout.write("  ## WARNING: CALL TO AERO-S IGNORED.\n");       
+        
+        # Calculate gradients if necessary
+        if nozzle.gradientsFlag == 1 and runAeroThermalStructuralGradients:
+    
+            if ( nozzle.gradientsMethod == 'ADJOINT' ):
+                
+                if gradCalc == 0: # i.e. failed adjoint calculation, use finite differences
+                    # XXX Ensure FD gradients work for 3-D nozzle. If this
+                    # function needs to be changed, ensure it is also changed
+                    # in the other 2 spots below too.
+                    multif.gradients.calcGradientsFD(nozzle,nozzle.fd_step_size,output);
+                else:
+                    # Check for other required gradients
+                    otherRequiredGradients = 0;
+                    for k in nozzle.gradients:
+                        if k not in ['MASS','VOLUME','MASS_WALL_ONLY','THRUST']:
+                            otherRequiredGradients = 1;
+                            sys.stderr.write(' ## WARNING: QoI gradients desired using ADJOINT '
+                              'method which do not have an associated adjoint calculation.\n'
+                              ' Namely: %s. The current implementation requires finite '
+                              'differencing across the aero analysis, so this method is '
+                              'equivalent in computational cost to choosing the FINITE_DIFF'
+                              ' method\n' % k);
+                    # Do finite difference for other QoI if there are any, but use
+                    # adjoint gradients for thrust
+                    if otherRequiredGradients:
+                        saveThrustGradients = nozzle.gradients['THRUST'];
+                        nozzle.gradients['THRUST'] = None;
+                        multif.gradients.calcGradientsFD(nozzle,nozzle.fd_step_size,output);
+                        nozzle.gradients['THRUST'] = saveThrustGradients;
+                        
+            elif ( nozzle.gradientsMethod == 'FINITE_DIFF' ):
+            
+                multif.gradients.calcGradientsFD(nozzle,nozzle.fd_step_size,output);  
+                         
+            else:
+                sys.stderr.write('  ## ERROR : Unknown gradients computation '
+                  'method.\n');
+                sys.exit(1);
+                
+         # Write separate gradients file
+            gradFile = open(nozzle.gradientsFile,'w');
+            for k in nozzle.outputTags:
+                np.savetxt(gradFile,nozzle.gradients[k]);
+            gradFile.close();               
+    
+    # Write data
+    if writeToFile:
+        if nozzle.outputFormat == 'PLAIN':
+            nozzle.WriteOutputFunctions_Plain();
+        else:
+            nozzle.WriteOutputFunctions_Dakota();
+    
+    return 0;
     
     

multif/MEDIUMF/meshgeneration.py
@@ -84,7 +84,6 @@ def GenerateNozzleMesh (nozzle):
     
     # --- Call Gmsh
     
-    CallGmsh(nozzle);
     try :   
         CallGmsh(nozzle);
     except:
@@ -229,28 +228,28 @@ def Extract_Boundary_Vertices(mesh_name, pyRefs):
     
 def Get3Dto2DEquivArea(nozzle, xnew_tab):
     
-    from .. import nozzle as noz
+    #from .. import nozzle as noz
+    #
+    #geometry = noz.geometry;
+    #
+    #majoraxisTmp = geometry.Bspline(nozzle.wall.majoraxis.coefs);
+    #minoraxisTmp = geometry.Bspline(nozzle.wall.minoraxis.coefs);
+    #centerTmp = geometry.Bspline(nozzle.wall.centerline.coefs);
+    #
+    #majoraxisVal = majoraxisTmp.radius(xnew_tab);
+    #minoraxisVal = minoraxisTmp.radius(xnew_tab);
+    #
+    #fr1 = majoraxisTmp.radius
+    #fr2 = minoraxisTmp.radius
+    #fz  = centerTmp.radius
+    #
+    #params = np.zeros(100)
+    #params[0] = 0.099; # z crd of the cut at the throat
+    #params[1] = 0.122638;  # z crd of the flat exit (bottom of the shovel)
+    #params[3] = 0.0;        # x_throat 
+    #params[4] = 4.0;        # x_exit 
     
-    geometry = noz.geometry;
-    
-    majoraxisTmp = geometry.Bspline(nozzle.wall.majoraxis.coefs);
-    minoraxisTmp = geometry.Bspline(nozzle.wall.minoraxis.coefs);
-    centerTmp = geometry.Bspline(nozzle.wall.centerline.coefs);
-    
-    majoraxisVal = majoraxisTmp.radius(xnew_tab);
-    minoraxisVal = minoraxisTmp.radius(xnew_tab);
-    
-    fr1 = majoraxisTmp.radius
-    fr2 = minoraxisTmp.radius
-    fz  = centerTmp.radius
-    
-    params = np.zeros(100)
-    params[0] = 0.099; # z crd of the cut at the throat
-    params[1] = 0.122638;  # z crd of the flat exit (bottom of the shovel)
-    params[3] = 0.0;        # x_throat 
-    params[4] = 4.0;        # x_exit 
-    
-    ynew_tab = multif.HIGHF.MF_GetRadius (xnew_tab, fr1, fr2, fz, params)
+    ynew_tab = multif.HIGHF.MF_GetRadius (xnew_tab, nozzle)
     
     return ynew_tab;
 

multif/nozzle/nozzle.py
@@ -1605,14 +1605,16 @@ class Nozzle:
                 
                 xi = nozzle.wall.centerline.coefs[0];
                 inletRadius = np.sqrt(majoraxisTmp.radius(xi)*minoraxisTmp.radius(xi));
-                params = np.zeros(5);
-                params[0] = inletRadius*np.sin(nozzle.wall.shovel_start_angle*np.pi/180.);
-                params[1] = nozzle.wall.centerline.coefs[-1] + nozzle.wall.shovel_height;
-                #params[2]                
-                params[3] = nozzle.xinlet;
-                params[4] = nozzle.xoutlet;
-                                
-                equivRadius = multif.HIGHF.MF_GetRadius (x, fr1, fr2, fz, params)
+                #params = np.zeros(5);
+                #params[0] = inletRadius*np.sin(nozzle.wall.shovel_start_angle*np.pi/180.);
+                #params[1] = nozzle.wall.centerline.coefs[-1] + nozzle.wall.shovel_height;
+                ##params[2]                
+                #params[3] = nozzle.xinlet;
+                #params[4] = nozzle.xoutlet;
+                #equivRadius = multif.HIGHF.MF_GetRadius (x, fr1, fr2, fz, params)
+                
+                equivRadius = multif.HIGHF.MF_GetRadius (x, nozzle);
+                
                 shape2d = np.transpose(np.array([x,equivRadius]))
                 nozzle.wall.geometry = geometry.PiecewiseLinear(shape2d);	
                 