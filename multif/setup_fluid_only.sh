f20b643f3a7445dea5b13988688bcdcb604e4b42
Vishal Srivastava <vsriv@umich.edu>
2017-10-06 11:41:03 -0400

    Added a bash script to compile MULTIF along with executables for the ML code

---
+++ multif/setup_fluid_only.sh
@@ -0,0 +1,3 @@
+python setup_fluid_only.py build_ext --inplace
+g++ -o ../example/ML_test/postproc ../example/ML_test/postproc.cpp
+g++ -o ../example/ML_test/make_responses ../example/ML_test/make_responses.cpp
