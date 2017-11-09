44a5fff66010e7f3a7383757547e4eb6f6576655
Rick Fenrich <rfenrich@stanford.edu>
2017-11-08 16:04:27 -0800

    revert use of deformed mesh

--- regression.py
+++ regression.py
@@ -5,6 +5,7 @@ the following sets:
 *Test everything
 *Test low-fidelity analysis only (2D and 3D parameterizations)
 *Test medium-fidelity analysis only (2D and 3D parameterizations)
+*Test high-fidelity analysis only (3D parameterization)
 *Test medium-fidelity adjoint cability (2D parameterization only)
 *Test finite difference gradient capability
 *Test linear and nonlinear FEA for 2D parameterization
@@ -160,19 +161,19 @@ if( test['all'] or test['medfi'] ):
     medfi_3dparam.diff_tol = 1e-6;
     test_list.append(medfi_3dparam);
 
-# ----------------------- 3D param, 2D dim, med-fi ----------------------------
-if( test['all'] or test['medfi'] ):
+# # ----------------------- 3D param, 2D dim, med-fi ----------------------------
+# if( test['all'] or test['medfi'] ):
 
-    medfi_3dparam_rans = TestCase('medfi_3dparam_rans');
-    medfi_3dparam_rans.description = 'General, 3D->2D, parallel, med-fi (RANS) analysis';
-    medfi_3dparam_rans.cfg_dir = 'example';
-    medfi_3dparam_rans.cfg_file = 'general-3d.cfg';
-    medfi_3dparam_rans.input_file = 'general-3d.in';
-    medfi_3dparam_rans.compare_file = 'example/regression/medfi_3dparam_rans.out';
-    medfi_3dparam_rans.fidelity = 8;
-    medfi_3dparam_rans.nproc = nCores;
-    medfi_3dparam_rans.diff_tol = 1e-6;
-    test_list.append(medfi_3dparam_rans);
+#     medfi_3dparam_rans = TestCase('medfi_3dparam_rans');
+#     medfi_3dparam_rans.description = 'General, 3D->2D, parallel, med-fi (RANS) analysis';
+#     medfi_3dparam_rans.cfg_dir = 'example';
+#     medfi_3dparam_rans.cfg_file = 'general-3d.cfg';
+#     medfi_3dparam_rans.input_file = 'general-3d.in';
+#     medfi_3dparam_rans.compare_file = 'example/regression/medfi_3dparam_rans.out';
+#     medfi_3dparam_rans.fidelity = 8;
+#     medfi_3dparam_rans.nproc = nCores;
+#     medfi_3dparam_rans.diff_tol = 1e-6;
+#     test_list.append(medfi_3dparam_rans);
 
 # ------------------ 2D param, 2D dim med-fi, adjoint gradients ---------------
 if( test['all'] or test['medfi'] or test['adjoint'] ):
