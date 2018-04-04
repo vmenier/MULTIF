2ab8c07cadcc9a61f579c2f877ba5f1f516fb123
Rick Walter Fenrich <rfenrich@sh-ln04.stanford.edu>
2018-04-03 18:53:42 -0700

    small bug fix for include file in lofi nozzle code

--- multif/models/aero/LOWF/lofinozzle.c
+++ multif/models/aero/LOWF/lofinozzle.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-#include "../meshutils/piecewise.h"
+#include "../../meshutils/piecewise.h"
 #include "odeint.h"
 
 #define PI 3.14159265358979323846
